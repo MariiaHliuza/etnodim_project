d5cbdfe26c2a46a1d0d0481398366696dd09bc93
MariiaHliuza <mariia.hliuza@nure.ua>
2024-04-02 16:13:48 +0200

    main

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-       $(document).ready(function(){
-        $("#dropDownIdSize").click(function(){
-          $("#dropDownIdSize").toggleClass("filter-block__button--hide");
-          $("#DropDownListIdSize").slideToggle("slow");
-        });
-      });
-
-      $(document).ready(function(){
-        $("#dropDownIdColor").click(function(){
-          $("#dropDownIdColor").toggleClass("filter-block__button--hide");
-          $("#DropDownListIdColor").slideToggle("slow");
-        });
-      });
+function amountCalculateIncrease(){
+    var x, y;
+    x = 1;
+    y = x + 1;
+    document.getElementById("amountProduct").innerHTML = y;
+}
 
